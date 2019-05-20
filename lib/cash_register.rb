578e7282ea4d88e5746921968d86da6bc869656e
Jason  Montes <jasone.montes@gmail.com>
2019-05-20 15:12:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,12 +16,12 @@ def add_item(title, price, quantity = 1)
   end
 end
 
-def apply_discount
-        if @discount
-            "After the discount, the total comes to $#{@total -= (@total * (@discount/100.0)).to_i}."
-          else "There is no discount to apply."
-        end
-    end
-  end
+# def apply_discount
+#         if @discount
+#             "After the discount, the total comes to $#{@total -= (@total * (@discount/100.0)).to_i}."
+#           else "There is no discount to apply."
+#         end
+#     end
+#   end
     
    
